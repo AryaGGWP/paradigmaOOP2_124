d768a0f9d2815b3dbeb9581c50eadd6868d5f9d8
KeikooUwU <127074149+KeikooUwU@users.noreply.github.com>
2023-05-29 10:41:31 +0700

    menambahkan class orang

--- ProblemMultiwaris/ProblemMultiwaris.cpp
+++ ProblemMultiwaris/ProblemMultiwaris.cpp
@@ -2,9 +2,17 @@
 using namespace std;
 
 class orang {
-    public
+public:
+    int umur;
 
+    orang(int pUmur):
+        umur(pUmur)
+    {
+        cout << "orang dibuat dengan umur" << umur << "\n" << endl;
+    }
 };
+
+
 
 
 int main()
