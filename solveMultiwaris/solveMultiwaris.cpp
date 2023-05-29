26ee4bbf82779b0a5d38d829d86a9c086b01ae05
KeikooUwU <127074149+KeikooUwU@users.noreply.github.com>
2023-05-29 11:07:29 +0700

    menambahkan class pelajar

--- solveMultiwaris/solveMultiwaris.cpp
+++ solveMultiwaris/solveMultiwaris.cpp
@@ -1,7 +1,25 @@
-// solveMultiwaris.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+using namespace std;
+
+class orang {
+public:
+    int umur;
+
+    orang(int pUmur)
+    {
+        cout << "orang dibuat dengan umur " << umur << "\n" << endl;
+    }
+};
+
+class pekerja : virtual public orang {
+public:
+
+    pekerja(int pUmur) :
+        orang(pUmur)
+    {
+        cout << "pekerja dibuat\n" << endl;
+    }
+};
 
 int main()
 {
