ec00b41219c7af4b23b04b3498e643c7ea615e43
KeikooUwU <127074149+KeikooUwU@users.noreply.github.com>
2023-05-29 22:06:38 +0700

    menambahkan class baseClass

--- noPewarisan/noPewarisan.cpp
+++ noPewarisan/noPewarisan.cpp
@@ -1,7 +1,13 @@
-// noPewarisan.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+using namespace std;
+
+class baseClass {
+public:
+	virtual void perkenalan() {
+		cout << "Hallo saya Function dari base class";
+	}
+};
+
 
 int main()
 {
